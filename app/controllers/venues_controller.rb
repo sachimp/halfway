cb5e5b9f10e31bef1b0803fff536710e3dbb824b
Sachin Gandhi <sachingandhi@live.com>
2022-03-01 18:13:40 +0000

    tried to implement geocoding based on address using the geocoder gem. It worked for a bit, but something has broken it...

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,3 +3,7 @@ class VenuesController < ApplicationController
     @venues = Venue.all
   end
 end
+
+# REMEMBER THIS GEOCODER METHOD ----
+# Flat.near("Tour Eiffel", 10)      # venues within 10 km of Tour Eiffel
+# Flat.near([40.71, 100.23], 20)    # venues within 20 km of a point
