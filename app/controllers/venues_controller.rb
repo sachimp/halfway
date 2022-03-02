ea4449cc1735d91f836d9db9033ddc7a0b5f7cf8
Sachin Gandhi <sachingandhi@live.com>
2022-03-02 11:34:29 +0000

    added code to controllers and styles to application.scss

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -6,9 +6,6 @@ class VenuesController < ApplicationController
         lat: flat.latitude,
         lng: flat.longitude
       }
+    end
   end
 end
-
-# REMEMBER THIS GEOCODER METHOD ----
-# Flat.near("Tour Eiffel", 10)      # venues within 10 km of Tour Eiffel
-# Flat.near([40.71, 100.23], 20)    # venues within 20 km of a point
