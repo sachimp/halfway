eea68200f0495049352b00261ac874ec4f1c641f
Sachin Gandhi <sachingandhi@live.com>
2022-03-02 11:31:13 +0000

    added my mapbox key, createdmapbox controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,11 @@
 class VenuesController < ApplicationController
   def index
     @venues = Venue.all
+    @markers = @venues.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
   end
 end
 
