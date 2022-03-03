fb20b9997e6519355c5d00b73622113790d9f101
Jimmy Yuen <jimmehyuen@gmail.com>
2022-03-03 12:39:38 +0000

    mapbox controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
- generate-venues-controller
+#  generate-venues-controller
   resources :venues, only: [:index, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
