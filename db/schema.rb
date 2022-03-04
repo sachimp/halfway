838d5ebc1e72e6c50032dba779498271f4103649
Sachin Gandhi <sachingandhi@live.com>
2022-03-04 12:06:06 +0000

    changed the seed file to soho bars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_165835) do
+ActiveRecord::Schema.define(version: 2022_03_04_114149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2022_03_01_165835) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "website"
+    t.string "image_url"
   end
 
   add_foreign_key "favourites", "users"
