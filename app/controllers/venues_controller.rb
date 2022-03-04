68e5b0b069e6e280164cc8c49c19cd714d94d518
Jimmy Yuen <jimmehyuen@gmail.com>
2022-03-04 12:52:35 +0000

    marker done

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -14,22 +14,29 @@ class VenuesController < ApplicationController
     halfway_long = (results1_long + results2_long).fdiv(2)
     @halfway_coordinates = [halfway_lat, halfway_long]
 
+    halfway_address = Geocoder.search(@halfway_coordinates)
     @venues_close = Venue.near(@halfway_coordinates, 1)
 
+    # all markers except halfway
     @markers = @venues_close.geocoded.map do |venue|
       {
         lat: venue.latitude,
         lng: venue.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { venue: venue })
+        info_window: render_to_string(partial: "info_window", locals: { venue: venue }),
+        image_url: helpers.asset_url("drinks.jpg")
       }
     end
 
-    @markers.push({
-      halfway: true,
-      lat: @halfway_coordinates.first,
-      lng: @halfway_coordinates.last,
-      info_window: render_to_string(partial: "halfway_window", locals: { venue: @halfway_coordinates })
-      })
+    # halfway marker
+    @markers.push(
+      {
+        halfway: true,
+        lat: @halfway_coordinates.first,
+        lng: @halfway_coordinates.last,
+        info_window: render_to_string(partial: "halfway_window", locals: { halfpoint: halfway_address }),
+        image_url: helpers.asset_url("coffee.jpg")
+      }
+    )
   end
 
   def show
