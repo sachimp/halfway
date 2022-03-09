6da064bfd83126bf133981abd345fa1523db040c
Raphaelle Manet <raphaelle.manet1@gmail.com>
2022-03-09 15:08:07 +0000

    Added some hard values for things not breaking in the demo

--- app/javascript/controllers/disable_button_controller.js
+++ app/javascript/controllers/disable_button_controller.js
@@ -1,35 +1,35 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  static targets = ["address1", "address2"]
+// export default class extends Controller {
+//   static targets = ["address1", "address2"]
 
-  connect() {
-    console.log("Hello from our first Stimulus controller")
-  }
+//   connect() {
+//     console.log("Hello from our first Stimulus controller")
+//   }
 
-  disable(event) {
-    // event.preventDefault();
-    // console.log(event);
+//   disable(event) {
+//     // event.preventDefault();
+//     // console.log(event);
 
-    // console.log(this);
-    // console.log(this.address1Target);
-    // console.log(this.address1Target.value);
-    // console.log(this.address2Target);
+//     // console.log(this);
+//     // console.log(this.address1Target);
+//     // console.log(this.address1Target.value);
+//     // console.log(this.address2Target);
 
-    if (this.address1Target.value == "" || this.address2Target.value == "") {
-      event.preventDefault();
-      console.log("HAHHAHAH, can't submit this");
-      //alert('You need to input addresses')
-    } else {
-      console.log("condition is happy");
-    }
+//     if (this.address1Target.value == "" || this.address2Target.value == "") {
+//       event.preventDefault();
+//       console.log("HAHHAHAH, can't submit this");
+//       //alert('You need to input addresses')
+//     } else {
+//       console.log("condition is happy");
+//     }
 
-  }
-  retry(event) {
-  //   const addresses = document.querySelector(".mapboxgl-ctrl-geocoder--input");
-  // console.log(addresses);
+//   }
+//   retry(event) {
+//   //   const addresses = document.querySelector(".mapboxgl-ctrl-geocoder--input");
+//   // console.log(addresses);
 
-    console.log("i am the upcoming change");
-    console.log(event);
-  }
-}
+//     console.log("i am the upcoming change");
+//     console.log(event);
+//   }
+// }
