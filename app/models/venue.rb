ee07bde094bb319ec0311edd3a5b08dcbbd11fa6
Jimmy Yuen <jimmehyuen@gmail.com>
2022-05-09 17:31:48 +0100

    fixed address input1 dropdown hidden behind address 2

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,6 @@ class Venue < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_many :favourites, dependent: :destroy
+  validates :address1, presence: true, allow_blank: false
+  validates :address2, presence: true
 end
